327ad87a4c7d179c2a52d22807aeba36b1f6b269
Yong Cho Yin <choyiny@users.noreply.github.com>
2020-01-19 14:51:04 -0500

    configure sentry on frontend

--- blog-frontend/src/app/interceptors/sentry.error-handler.ts
+++ blog-frontend/src/app/interceptors/sentry.error-handler.ts
@@ -1,13 +1,16 @@
 import { ErrorHandler, Injectable } from '@angular/core';
 import * as Sentry from '@sentry/browser';
 import {environment} from '../../environments/environment';
+// @ts-ignore
+import { version } from '../../../package.json';
 
 @Injectable()
 export class SentryErrorHandler implements ErrorHandler {
   constructor() {
     if (!!environment.settings.sentry_dsn) {
       Sentry.init({
-        dsn: environment.settings.sentry_dsn
+        dsn: environment.settings.sentry_dsn,
+        release: 'blog-frontend@' + version
       });
     }
   }
