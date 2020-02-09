8d8e615ffccfba7a7727ecc6601af54bb47aa34e
Yong Cho Yin <choyiny@users.noreply.github.com>
2020-02-09 00:56:31 -0500

    continuous deployment

---
+++ .github/actions/deploy/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo -n "$VAULT_PASS" > ~/password.secret
+mkdir ~/.ssh
+echo "$DEPLOY_KEY" > ~/.ssh/id_rsa
+chmod 0600 ~/.ssh/id_rsa
+
+ansible-playbook -i ansible/inventory -u choyiny -b --vault-password-file ~/password.secret ansible/$(echo $DEPLOY_FILE) --extra-vars="frontend_image=$IMAGE_URL"
