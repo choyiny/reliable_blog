d7ad80f4b47fb62763d7a16be4fb87562adcb8ee
Jordan Liu <liu.jordan.com@gmail.com>
2020-02-25 15:35:33 -0500

    ASDF

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -15,4 +15,4 @@ class ApplicationController < ActionController::API
     Raven.user_context(id: session[:current_user_id]) # or anything else in session
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
-end
+end 
