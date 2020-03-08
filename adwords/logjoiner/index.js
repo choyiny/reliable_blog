0ba1ba31c9b9a42e203b637127217d32a6df7546
Jordan Liu <liu.jordan.com@gmail.com>
2020-03-08 18:22:50 -0400

    did some changes

--- adwords/logjoiner/index.js
+++ adwords/logjoiner/index.js
@@ -1,26 +1,15 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const pg = require('pg')
-const QueryStream = require('pg-query-stream')
-const JSONStream = require('JSONStream')
+const redis = require("redis");
 
 const queryStoreUrl = 'mongodb://root:example@localhost:27017';
 const clickMapUrl = 'mongodb://root:example@localhost:27018';
 
 const dbName = 'myproject';
-
-function makeid(length) {
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-   var charactersLength = characters.length;
-   for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
-}
-
-const query_id = makeid(20)
-const ad_id = makeid(20)
+const subscriber = redis.createClient();
+ 
+const query_id = "asdfasdfasdfasdf"
+const ad_id = "asdfasdfasdfasdfasdf"
 const time = new Date().getTime()
 
 const fakeQuery = {
@@ -35,11 +24,15 @@ const fakeQuery = {
     attr2: "attr2"
   }
 }
-const fakeClick = {
-  query_id: query_id,
-  time: time,
-  ad_id: ad_id
+
+function makeClick() {
+  return {
+    query_id: query_id,
+    time: new Date().getTime(),
+    ad_id: ad_id
+  }
 }
+
 // shared state
 var Share = function() {
   this.things = []
@@ -57,20 +50,11 @@ var Share = function() {
 }
 const share = new Share()
 
-const client = new pg.Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'clicks',
-  password: 'example',
-  port: 5432,
-})
-
 MongoClient.connect(queryStoreUrl, function(err, client) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
  
   const db = client.db(dbName);
-  db.collection('asdf').findPne
   // on new click logs, join with a query and send to clickmap
   share.on('newclicklog', data => {
     share.emit('pushtoclickmap', {query: fakeQuery, clicklog: data})
@@ -85,16 +69,14 @@ MongoClient.connect(clickMapUrl, function(err, client) {
   console.log("Connected successfully to server");
  
   const db = client.db(dbName);
-  let collection = db.collection('asdf')
 
   // on new joined click+query send to click map
   share.on('pushtoclickmap', data => {
-    db.collection('asdf').findOneAndUpdate(
+    db.collection('asdf').updateOne(
       {
         query_id: data.query._id,
         search_term: data.query.search_term
       },
-      [['ad_id','asc']],
       {$addToSet: {clicklogs: data.clicklog}},
       {upsert: true}  
     )
@@ -104,22 +86,11 @@ MongoClient.connect(clickMapUrl, function(err, client) {
   share.on('end', () => {client.close()})
 });
 
-client.connect((err) => {
-  if (err) throw err;
-  const query = new QueryStream('SELECT * FROM generate_series(0, $1) num', [1000])
-  const stream = client.query(query)
-
-  // release the client when the stream is finished
-  stream.on('end', () => {
-    client.end()
-    share.emit('end')
-  })
-
-  // emit all new data
-  stream.on('data', data => {
-    share.emit('newclicklog', fakeClick)
-  })
-})
+subscriber.on("message", function (channel, message) {
+  console.log("Message: " + message + " on channel: " + channel + " is arrive!");
+  share.emit('newclicklog', JSON.parse(message))
+});
+subscriber.subscribe("notification");
 
 
 
