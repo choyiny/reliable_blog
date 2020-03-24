47c1a00c8a32cb440f76a1a4c788522ce11385e1
Jordan Liu <liu.jordan.com@gmail.com>
2020-03-24 00:49:02 -0400

    haha push

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_235555) do
+ActiveRecord::Schema.define(version: 2020_03_24_040710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_235555) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
+    t.string "surname"
   end
 
   add_foreign_key "comments", "posts"
