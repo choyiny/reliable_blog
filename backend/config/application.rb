b086247bcabcc58abd4f707bc042357edcc0e0fe
Yong Cho Yin <choyiny@users.noreply.github.com>
2020-03-23 23:19:40 -0400

    sentry dsn optional

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -37,7 +37,7 @@ module Backend
 
     config.elastic_apm.disable_send = !Rails.env.production?
 
-    if Rails.env.production?
+    if Rails.env.production? && ENV['SENTRY_DSN']
       Raven.configure do |config|
         config.dsn = ENV['SENTRY_DSN']
       end
